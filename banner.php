a33c07ef7f93fe6fc5ffece46e322a1ff4033b0a
Phyo AungThant <phyoaungthant@gmail.com>
2021-11-10 01:05:57 +0800

    Add files for Banner

---
+++ banner.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style href="css/banner.css"></style>
+    </head>
+    <body>
+        <div id="banner">
+            <div id="company-name"><a href="mainMenu.php" style="text-decoration: none; color: inherit;">X-Fab</a></div>
+            <span id="greetings">
+                <?php
+                    echo ("Welcome! ");
+                ?>
+            </span>
+            <div style="float: clear;"></div>
+        </div>
+    </body>
+</html>
---
+++ bannerMenu.php
@@ -0,0 +1,30 @@
+<head>
+<link rel="stylesheet" href="css/banner.css" type="text/css"/>
+</head>
+<body>
+	<ul class="menu">
+		<li class="dropdown">
+			<a class="dropbtn">Table Input &nabla;</a>
+			<div class="dropdown-content">
+				<a href="viewOrder.php">View Orders</a>
+				<a href="addOrder.php">Add Order</a>
+			</div>
+		</li>
+		
+		<li class="dropdown">
+			<a class="dropbtn">Device Length &nabla;</a>
+			<div class="dropdown-content">
+				<a href="#">View Device Lengths</a>
+				<a href="#">Add Device Length</a>
+			</div>
+		</li>
+		
+		<li class="dropdown">
+			<a class="dropbtn">Module Category &nabla;</a>
+			<div class="dropdown-content">
+				<a href="#">View Module Categories</a>
+				<a href="#">Add Module Category</a>
+			</div>
+		</li>
+	</ul>
+</body>
