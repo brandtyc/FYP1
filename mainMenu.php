99a7527bb40d172fa93aeb602cd092973cffa71e
Phyo AungThant <phyoaungthant@gmail.com>
2021-11-10 01:06:55 +0800

    Main menu of the system

---
+++ mainMenu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>X-Fab</title>
+    </head>
+    <body>        
+        <?php
+            require_once("banner.php");
+            require_once("bannerMenu.php");
+        ?>
+        
+        <!--This is the menu where the user will select-->
+        <p><i>This is the main page of the X-Fab form web-based system. Please choose from the the above menus to choose the action.</i></p>
+    </body>
+</html>
